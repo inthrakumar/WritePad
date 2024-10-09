790eca8ae96260f74264eb08f4f7627a8abf71a7
inthrakumar <inthrak04@gmail.com>
2024-10-09 21:03:43 +0530

    setting up user identification in id tokens

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -1,13 +1,19 @@
 import { Liveblocks } from '@liveblocks/node';
 import { env_varaibles } from '@/config/envconfig';
 import { PrismaClient } from '@prisma/client';
+import { getUserColors } from '@/utils/UserUtils';
 const liveblocks = new Liveblocks({
   secret: env_varaibles.LIVEBLOCKS_SECRET_KEY!,
 });
 const prisma = new PrismaClient();
 export async function POST(req: Request) {
-  // Parse the request body
   const req_body = await req.json();
+  if (!req_body.user_id) {
+    return new Response(JSON.stringify({ error: 'User ID is required' }), {
+      status: 400,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
   const user = await prisma.user.findUnique({
     where: {
       userid: req_body.user_id,
@@ -15,16 +21,26 @@ export async function POST(req: Request) {
   });
 
   if (!user) {
-    return new Response(JSON.stringify({ error: 'User not found' }), {
+    return new Response(JSON.stringify({ error: 'User not authenticated' }), {
       status: 400,
       headers: { 'Content-Type': 'application/json' },
     });
   }
-  // const { status, body } = await liveblocks.identifyUser({
-  //   userId: user_id,
-  // });
-
-  return new Response(JSON.stringify({ message: 'Hello from Next.js!' }), {
+  const colorArray = getUserColors();
+  const { status, body } = await liveblocks.identifyUser(
+    {
+      userId: user.userid,
+      groupIds: [],
+    },
+    {
+      userInfo: {
+        name: user.username,
+        avatar: `https://api.dicebear.com/9.x/initials/svg?seed=${user.username}`,
+        colors: colorArray,
+      },
+    }
+  );
+  return new Response(JSON.stringify({ message: 'User Authenticated' }), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
   });
