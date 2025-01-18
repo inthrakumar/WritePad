5220f00115e5d5d12ddfa414db59d887bdff42d9
ik <ik@inthrakumar.edu>
2025-01-18 17:34:57 +0530

    add a user profile page for the signed in user

--- src/scenes/Header.tsx
+++ src/scenes/Header.tsx
@@ -15,89 +15,100 @@ import { useUser } from '@clerk/clerk-react';
 import { getRandomAnonymousName } from '../utils/AnonymousUtils';
 
 import {
-    Tooltip,
-    TooltipContent,
-    TooltipProvider,
-    TooltipTrigger,
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
 } from '@/components/ui/tooltip';
 
 function Header() {
-    const user = useUser();
+  const user = useUser();
 
-    let userName =
-        user.user?.fullName || user.user?.firstName || user.user?.lastName;
-    if (!user.isSignedIn) userName = getRandomAnonymousName();
+  let userName =
+    user.user?.fullName || user.user?.firstName || user.user?.lastName;
+  if (!user.isSignedIn) userName = getRandomAnonymousName();
 
-    const { signOut } = useClerk();
-    const router = useRouter();
-    const handleSignOut = async () => {
-        try {
-            await signOut({ redirectUrl: '/' });
-        } catch (error) { }
-    };
-    return (
-        <div className="max-w-10xl w-[100vw] font-poppins flex justify-between items-center fixed top-0  max-h-[75px] shadow-sm bg-white ">
-            <Link href={'/'}>
-                <div className="flex items-center justify-center gap-4">
-                    <span>
-                        <Image src={logo} alt="logo" width={75} height={75} />
-                    </span>
-                    <h1 className="text-4xl tracking-tight text-orange-700">
-                        Write <span className="text-black">Pad</span>
-                    </h1>
-                </div>
-            </Link>
-            <div className="flex text-lg items-center justify-center gap-5 mr-8">
-                <Link href={'/documents'}>
-                    <span className="tracking-tight hover:text-orange-700 ">My Docs</span>
-                </Link>
-                <Link href={'/shared-docs'}>
-                    <span className="tracking-tight hover:text-orange-700 ">Shared</span>
-                </Link>
+  const { signOut } = useClerk();
+  const router = useRouter();
+  const handleSignOut = async () => {
+    try {
+      await signOut({ redirectUrl: '/' });
+    } catch (error) {
+      console.error('Error in signing out');
+    }
+  };
 
-                <TooltipProvider>
-                    <Tooltip>
-                        <TooltipTrigger>
-                            <Avatar>
-                                {user.isSignedIn ? (
-                                    <AvatarImage
-                                        src={`https://api.dicebear.com/9.x/initials/svg?seed=${userName}`}
-                                    ></AvatarImage>
-                                ) : (
-                                    <AvatarImage
-                                        src={`https://api.dicebear.com/9.x/initials/svg?seed=${userName}`}
-                                    ></AvatarImage>
-                                )}
-                            </Avatar>
-                        </TooltipTrigger>
-                        <TooltipContent>
-                            <p>{userName}</p>
-                        </TooltipContent>
-                    </Tooltip>
-                </TooltipProvider>
+  const handleUserProfileRedirect = () => {
+    if (user.isSignedIn) {
+      router.push('/user-profile');
+    } else {
+      router.push('/sign-in');
+    }
+  };
 
-                <div>
-                    <Authenticated>
-                        <Button onClick={handleSignOut}>Sign Out</Button>
-                    </Authenticated>
-                    <Unauthenticated>
-                        <Button
-                            onClick={() => {
-                                router.push('/sign-in');
-                            }}
-                        >
-                            SignIn
-                        </Button>
-                    </Unauthenticated>
-                    <AuthLoading>
-                        <Button>
-                            <RiLoader5Line size={30} className="animate-spin" />
-                        </Button>
-                    </AuthLoading>
-                </div>
-            </div>
+  return (
+    <div className="max-w-10xl w-[100vw] font-poppins flex justify-between items-center fixed top-0  max-h-[75px] shadow-sm bg-white ">
+      <Link href={'/'}>
+        <div className="flex items-center justify-center gap-4">
+          <span>
+            <Image src={logo} alt="logo" width={75} height={75} />
+          </span>
+          <h1 className="text-4xl tracking-tight text-orange-700">
+            Write <span className="text-black">Pad</span>
+          </h1>
         </div>
-    );
+      </Link>
+      <div className="flex text-lg items-center justify-center gap-5 mr-8">
+        <Link href={'/documents'}>
+          <span className="tracking-tight hover:text-orange-700 ">My Docs</span>
+        </Link>
+        <Link href={'/shared-docs'}>
+          <span className="tracking-tight hover:text-orange-700 ">Shared</span>
+        </Link>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger>
+              <Avatar onClick={handleUserProfileRedirect}>
+                {user.isSignedIn ? (
+                  <AvatarImage
+                    src={`https://api.dicebear.com/9.x/initials/svg?seed=${userName}`}
+                  ></AvatarImage>
+                ) : (
+                  <AvatarImage
+                    src={`https://api.dicebear.com/9.x/initials/svg?seed=${userName}`}
+                  ></AvatarImage>
+                )}
+              </Avatar>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{userName}</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <div>
+          <Authenticated>
+            <Button onClick={handleSignOut}>Sign Out</Button>
+          </Authenticated>
+          <Unauthenticated>
+            <Button
+              onClick={() => {
+                router.push('/sign-in');
+              }}
+            >
+              SignIn
+            </Button>
+          </Unauthenticated>
+          <AuthLoading>
+            <Button>
+              <RiLoader5Line size={30} className="animate-spin" />
+            </Button>
+          </AuthLoading>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
