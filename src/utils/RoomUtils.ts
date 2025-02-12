733c969ce2edbfc66be643858821cda461d70d33
inthrakumar <inthrak04@gmail.com>
2025-02-12 14:38:21 +0530

    change in roomutils to update the shared users in convex

--- src/utils/RoomUtils.ts
+++ src/utils/RoomUtils.ts
@@ -5,293 +5,298 @@ import { nanoid } from 'nanoid';
 import { revalidatePath } from 'next/cache';
 import { auth } from '@clerk/nextjs/server';
 import {
-    liveblocks_connection,
-    convex_connection,
+  liveblocks_connection,
+  convex_connection,
 } from '@/config/serverconfig';
 import { api } from '../../convex/_generated/api';
 import {
-    MoveFile,
-    DeleteFile,
-    DeleteFolder,
-    UpdateTitle,
-    MoveFolder,
+  MoveFile,
+  DeleteFile,
+  DeleteFolder,
+  UpdateTitle,
+  MoveFolder,
 } from '../types/types';
 import { Id } from '../../convex/_generated/dataModel';
 import { isDeleted } from 'yjs';
+import { Trykker } from 'next/font/google';
 const CreateRoom = async ({
-    userId,
-    email,
-    title,
-    type,
-    parent,
+  userId,
+  email,
+  title,
+  type,
+  parent,
 }: {
-    userId: string;
-    email: string;
-    title: string;
-    type: string;
-    parent: string;
+  userId: string;
+  email: string;
+  title: string;
+  type: string;
+  parent: string;
 }) => {
-    auth().protect();
-    const roomId = nanoid();
-    try {
-        const data = {
-            owner: userId,
-            email: email,
-            title: title,
-            isAlive: 'true',
-        };
-        let roomDetails;
-        if (type !== 'folder') {
-            const usersAccesses: RoomAccesses = {
-                [email]: ['room:write'],
-            };
-            roomDetails = await liveblocks_connection.createRoom(roomId, {
-                usersAccesses,
-                defaultAccesses: [],
-                metadata: data,
-            });
-        }
-
-        const convexRoom = await convex_connection.mutation(api.rooms.createRoom, {
-            parent: parent,
-            roomTitle: title,
-            roomId,
-            userid: userId,
-            type: type,
-        });
-        revalidatePath(`/`);
-        return JSON.parse(JSON.stringify(convexRoom));
-    } catch (error) {
-        return null;
+  auth().protect();
+  const roomId = nanoid();
+  try {
+    const data = {
+      owner: userId,
+      email: email,
+      title: title,
+      isAlive: 'true',
+    };
+    let roomDetails;
+    if (type !== 'folder') {
+      const usersAccesses: RoomAccesses = {
+        [email]: ['room:write'],
+      };
+      roomDetails = await liveblocks_connection.createRoom(roomId, {
+        usersAccesses,
+        defaultAccesses: [],
+        metadata: data,
+      });
     }
+
+    const convexRoom = await convex_connection.mutation(api.rooms.createRoom, {
+      parent: parent,
+      roomTitle: title,
+      roomId,
+      userid: userId,
+      type: type,
+    });
+    revalidatePath(`/`);
+    return JSON.parse(JSON.stringify(convexRoom));
+  } catch (error) {
+    return null;
+  }
 };
 
 const UpdateTitleFn = async ({ roomId, id, title }: UpdateTitle) => {
-    auth().protect();
-    try {
-        await liveblocks_connection.updateRoom(roomId, {
-            metadata: {
-                title,
-            },
-        });
-        const updatedRoom = await convex_connection.mutation(
-            api.rooms.UpdateTitle,
-            {
-                id: id,
-                title: title,
-            }
-        );
-        console.log(updatedRoom);
-        revalidatePath(`/`);
-        return JSON.parse(JSON.stringify(updatedRoom));
-    } catch (error) {
-        return null;
-    }
+  auth().protect();
+  try {
+    await liveblocks_connection.updateRoom(roomId, {
+      metadata: {
+        title,
+      },
+    });
+    const updatedRoom = await convex_connection.mutation(
+      api.rooms.UpdateTitle,
+      {
+        id: id,
+        title: title,
+      }
+    );
+    console.log(updatedRoom);
+    revalidatePath(`/`);
+    return JSON.parse(JSON.stringify(updatedRoom));
+  } catch (error) {
+    return null;
+  }
 };
 
 const getOwnerRooms = async ({ roomId }: { roomId: string }) => {
-    auth().protect();
-    try {
-        const roomDetails = await convex_connection.query(api.rooms.getUserRooms, {
-            roomId: roomId,
-        });
-        return JSON.parse(JSON.stringify(roomDetails));
-    } catch (error) {
-        console.error(error);
-    }
+  auth().protect();
+  try {
+    const roomDetails = await convex_connection.query(api.rooms.getUserRooms, {
+      roomId: roomId,
+    });
+    return JSON.parse(JSON.stringify(roomDetails));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const getRoom = async ({ roomId }: { roomId: string }) => {
-    auth().protect();
-    try {
-        const roomDetails = await liveblocks_connection.getRoom(roomId);
-        revalidatePath(`/`);
-        return JSON.parse(JSON.stringify(roomDetails));
-    } catch (error) {
-        console.error(error);
-    }
+  auth().protect();
+  try {
+    const roomDetails = await liveblocks_connection.getRoom(roomId);
+    revalidatePath(`/`);
+    return JSON.parse(JSON.stringify(roomDetails));
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 const updateUserAccess = async ({
-    emailList,
-    roomId,
-    accessType,
+  emailList,
+  roomId,
+  accessType,
 }: {
-    emailList: string[];
-    roomId: string;
-    accessType: 'write' | 'read';
+  emailList: string[];
+  roomId: string;
+  accessType: 'write' | 'read';
 }) => {
-    auth().protect();
+  auth().protect();
 
-    try {
-        const roomDetails = await liveblocks_connection.getRoom(roomId);
-        const usersAccesses = roomDetails.usersAccesses;
+  try {
+    const roomDetails = await liveblocks_connection.getRoom(roomId);
+    const usersAccesses = roomDetails.usersAccesses;
 
-        emailList.forEach((email) => {
-            usersAccesses[email] =
-                accessType === 'write'
-                    ? ['room:write']
-                    : ['room:read', 'room:presence:write'];
-        });
-        await liveblocks_connection.updateRoom(roomId, { usersAccesses });
-        revalidatePath(`/`);
+    emailList.forEach((email) => {
+      usersAccesses[email] =
+        accessType === 'write'
+          ? ['room:write']
+          : ['room:read', 'room:presence:write'];
+    });
+    await liveblocks_connection.updateRoom(roomId, { usersAccesses });
+    const response = await convex_connection.mutation(
+      api.rooms.AddSharedRooms,
+      {
+        userIds: emailList,
+        roomId,
+      }
+    );
+    if (!response.status)
+      return JSON.parse(
+        JSON.stringify({
+          message: 'Error in updating the user accesses',
+        })
+      );
+    revalidatePath(`/`);
 
-        await triggerNotifications(emailList, roomId, accessType);
+    await triggerNotifications(emailList, roomId, accessType);
 
-        return usersAccesses;
-    } catch (error) {
-        console.error('Failed to update user access:', error);
-        throw new Error('Error updating user access');
-    }
+    return usersAccesses;
+  } catch (error) {
+    console.error('Failed to update user access:', error);
+    throw new Error('Error updating user access');
+  }
 };
 
 const triggerNotifications = async (
-    emailList: string[],
-    roomId: string,
-    accessType: 'write' | 'read'
+  emailList: string[],
+  roomId: string,
+  accessType: 'write' | 'read'
 ) => {
-    const notificationPromises = emailList.map((email) =>
-        liveblocks_connection.triggerInboxNotification({
-            userId: email,
-            kind:
-                accessType === 'write' ? '$writeAccessGranted' : '$readAccessGranted',
-            subjectId: roomId,
-            activityData: {
-                accessType,
-                timestamp: Date.now(),
-            },
-            roomId,
-        })
-    );
+  const notificationPromises = emailList.map((email) =>
+    liveblocks_connection.triggerInboxNotification({
+      userId: email,
+      kind:
+        accessType === 'write' ? '$writeAccessGranted' : '$readAccessGranted',
+      subjectId: roomId,
+      activityData: {
+        accessType,
+        timestamp: Date.now(),
+      },
+      roomId,
+    })
+  );
 
-    await Promise.all(notificationPromises);
+  await Promise.all(notificationPromises);
 };
-
-const getSharedRooms = async (userId: string) => {
-    auth().protect();
-    try {
-        const {
-            data: sharedRooms,
-            nextCursor,
-            nextPage,
-        } = await liveblocks_connection.getRooms({
-            limit: 20,
-            userId,
-        });
-        const filteredsharedRooms = sharedRooms.filter((rooms) => {
-            return rooms.metadata.userid != userId;
-        });
-        console.log(filteredsharedRooms);
-
-        return JSON.parse(JSON.stringify(filteredsharedRooms));
-    } catch (error) {
-        console.error('Error fetching shared rooms:', error);
-        throw new Error('Failed to fetch shared rooms');
+const getSharedRooms = async (userId: string, page: number) => {
+  auth().protect();
+  try {
+    const response = await convex_connection.query(api.rooms.GetSharedRooms, {
+      userId,
+      page,
+    });
+    if (response.message) {
+      return JSON.parse(
+        JSON.stringify({
+          message: 'There is no shared rooms for the user',
+        })
+      );
     }
+    const sharedRooms = response.rooms;
+
+    return JSON.parse(JSON.stringify(sharedRooms));
+  } catch (error) {
+    console.error('Error fetching shared rooms:', error);
+    throw new Error('Failed to fetch shared rooms');
+  }
 };
 
 const DeleteRoom = async ({ roomId, id }: DeleteFile) => {
-    auth().protect();
-    try {
-        const room = await liveblocks_connection.updateRoom(roomId, {
-            metadata: {
-                isAlive: 'false',
-            },
-        });
-        const deletedRoom = await convex_connection.mutation(api.rooms.DeleteFile, {
-            id,
-        });
-        if (deletedRoom.success) {
-            return JSON.parse(
-                JSON.stringify({
-                    success: true,
-                })
-            );
-        }
-    } catch (error) {
-        console.error('Error in deleting the room:', error);
-        throw new Error('Error in deleting a file');
+  auth().protect();
+  try {
+    const room = await liveblocks_connection.updateRoom(roomId, {
+      metadata: {
+        isAlive: 'false',
+      },
+    });
+    const deletedRoom = await convex_connection.mutation(api.rooms.DeleteFile, {
+      id,
+    });
+    if (deletedRoom.success) {
+      return JSON.parse(
+        JSON.stringify({
+          success: true,
+        })
+      );
     }
+  } catch (error) {
+    console.error('Error in deleting the room:', error);
+    throw new Error('Error in deleting a file');
+  }
 };
 const DeleteFolderContents = async ({ id, url }: DeleteFolder) => {
-    auth().protect();
-    try {
-        const deletedRooms = await convex_connection.mutation(
-            api.rooms.DeleteFolder,
-            { id, url }
-        );
-        if (Array.isArray(deletedRooms.childIds)) {
-            await Promise.all(
-                deletedRooms.childIds.map((childId) =>
-                    liveblocks_connection.updateRoom(childId, {
-                        metadata: {
-                            isAlive: 'false',
-                        },
-                    })
-                )
-            );
-        }
-        return JSON.parse(
-            JSON.stringify({
-                sucess: true,
-            })
-        );
-    } catch (error) {
-        console.error('Error in deleting the folder:', error);
-        ``;
-        throw new Error('Error in deleting the folder');
+  auth().protect();
+  try {
+    const deletedRooms = await convex_connection.mutation(
+      api.rooms.DeleteFolder,
+      { id, url }
+    );
+    if (Array.isArray(deletedRooms.childIds)) {
+      await Promise.all(
+        deletedRooms.childIds.map((childId) =>
+          liveblocks_connection.updateRoom(childId, {
+            metadata: {
+              isAlive: 'false',
+            },
+          })
+        )
+      );
     }
+    return JSON.parse(
+      JSON.stringify({
+        sucess: true,
+      })
+    );
+  } catch (error) {
+    console.error('Error in deleting the folder:', error);
+    throw new Error('Error in deleting the folder');
+  }
 };
 
 const MoveFileContents = async ({ id, parenturl }: MoveFile) => {
-    auth().protect();
-    try {
-        const movedRoom = await convex_connection.mutation(api.rooms.MoveFile, {
-            id,
-            parenturl,
-        });
-        revalidatePath('/');
-        return JSON.parse(JSON.stringify(movedRoom));
-    } catch (error) {
-        throw new Error('Error in moving the file');
-    }
+  auth().protect();
+  try {
+    const movedRoom = await convex_connection.mutation(api.rooms.MoveFile, {
+      id,
+      parenturl,
+    });
+    revalidatePath('/');
+    return JSON.parse(JSON.stringify(movedRoom));
+  } catch (error) {
+    throw new Error('Error in moving the file');
+  }
 };
 const MoveFolderContents = async ({
-    id,
-    parenturl,
-    oldfileurl,
-    newfileurl,
+  id,
+  parenturl,
+  oldfileurl,
+  newfileurl,
 }: MoveFolder) => {
-    auth().protect();
-    try {
-        const movedFolder = await convex_connection.mutation(api.rooms.MoveFolder, {
-            id,
-            parenturl,
-            oldfileurl,
-            newfileurl,
-        });
-        revalidatePath('/');
-        return JSON.parse(JSON.stringify(movedFolder));
-    } catch (error) {
-        throw new Error('Error in moving the file');
-    }
+  auth().protect();
+  try {
+    const movedFolder = await convex_connection.mutation(api.rooms.MoveFolder, {
+      id,
+      parenturl,
+      oldfileurl,
+      newfileurl,
+    });
+    revalidatePath('/');
+    return JSON.parse(JSON.stringify(movedFolder));
+  } catch (error) {
+    throw new Error('Error in moving the file');
+  }
 };
-function toTitleCase(str: string) {
-    return str
-        .toLowerCase()
-        .split(' ')
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-        .join(' ');
-}
 export {
-    MoveFolderContents,
-    DeleteFolderContents,
-    DeleteRoom,
-    CreateRoom,
-    UpdateTitleFn,
-    getOwnerRooms,
-    getRoom,
-    updateUserAccess,
-    getSharedRooms,
-    MoveFileContents,
+  MoveFolderContents,
+  DeleteFolderContents,
+  DeleteRoom,
+  CreateRoom,
+  UpdateTitleFn,
+  getOwnerRooms,
+  getRoom,
+  updateUserAccess,
+  getSharedRooms,
+  MoveFileContents,
 };
