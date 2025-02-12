6b003820ce5f2c49c1e1db1b769df0d23e2d10e5
inthrakumar <inthrak04@gmail.com>
2025-02-12 22:26:16 +0530

    removal of modal from the doc page

--- src/scenes/CollabarativeEditor.tsx
+++ src/scenes/CollabarativeEditor.tsx
@@ -1,48 +1,45 @@
-"use client";
+'use client';
 
-
-import * as Y from "yjs";
-import { LiveblocksYjsProvider } from "@liveblocks/yjs";
-import { useRoom, useSelf } from "@liveblocks/react/suspense";
-import { useEffect, useState } from "react";
-import RoomTitle from "./RoomTitle";
-import { TiptapEditor } from "./TextEditor";
-import ShareModal from "./ShareModal";
-import CollabaratorStack from "./CollabaratorStack";
+import * as Y from 'yjs';
+import { LiveblocksYjsProvider } from '@liveblocks/yjs';
+import { useRoom, useSelf } from '@liveblocks/react/suspense';
+import { useEffect, useState } from 'react';
+import RoomTitle from './RoomTitle';
+import { TiptapEditor } from './TextEditor';
+import CollabaratorStack from './CollabaratorStack';
 export function CollaborativeEditor() {
-    const room = useRoom();
-    const [doc, setDoc] = useState<Y.Doc>();
-    const [provider, setProvider] = useState<any>();
+  const room = useRoom();
+  const [doc, setDoc] = useState<Y.Doc>();
+  const [provider, setProvider] = useState<any>();
 
-    useEffect(() => {
-        const yDoc = new Y.Doc();
-        const yProvider = new LiveblocksYjsProvider(room, yDoc);
-        setDoc(yDoc);
-        setProvider(yProvider);
+  useEffect(() => {
+    const yDoc = new Y.Doc();
+    const yProvider = new LiveblocksYjsProvider(room, yDoc);
+    setDoc(yDoc);
+    setProvider(yProvider);
 
-        return () => {
-            yDoc?.destroy();
-            yProvider?.destroy();
-        };
-    }, [room]);
+    return () => {
+      yDoc?.destroy();
+      yProvider?.destroy();
+    };
+  }, [room]);
 
-    if (!doc || !provider) {
-        return null;
-    }
+  if (!doc || !provider) {
+    return null;
+  }
 
-    return <div className="flex p-6 flex-col items-center justify-center gap-3">
-        <div className="flex max-h-[60vh] items-center justify-around w-[70vw]">
-            <RoomTitle />
-            <ShareModal />
-            <CollabaratorStack/>
-        </div>
-        <TiptapEditor doc={doc} provider={provider} />
-    </div>;
+  return (
+    <div className="flex p-6 flex-col items-center justify-center gap-3">
+      <div className="flex max-h-[60vh] items-center justify-around w-[70vw]">
+        <RoomTitle />
+        <CollabaratorStack />
+      </div>
+      <TiptapEditor doc={doc} provider={provider} />
+    </div>
+  );
 }
 
 type EditorProps = {
-    doc: Y.Doc;
-    provider: any;
+  doc: Y.Doc;
+  provider: any;
 };
-
-
